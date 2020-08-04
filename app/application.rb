a3e884de1e866d49f415f28bc17fe5fdc85184a0
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-08-04 18:28:20 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -11,8 +11,8 @@ class Application
       item = @@items.find{|i| i.name == item_name}
       resp.write item.price
     else
-      resp.status = 404
       resp.write "Route not found"
+      resp.status = 404
     end
     
     resp.finish
