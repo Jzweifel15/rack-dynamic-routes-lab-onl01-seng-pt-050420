00e2da620a30381aba5be116b98606ffb2b4fc06
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-08-04 18:27:42 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -9,7 +9,7 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last 
       item = @@items.find{|i| i.name == item_name}
-      resp.write item.price
+      resp.write item
     else
       resp.status = 404
       resp.write "Route not found"
